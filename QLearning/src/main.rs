8f35f1391f8d67abb7ae11835110aab448e4b81e
m-m-adams <52469396+m-m-adams@users.noreply.github.com>
2021-09-16 11:32:38 -0400

    formatting

--- QLearning/src/main.rs
+++ QLearning/src/main.rs
@@ -1,25 +1,23 @@
-use derive_more::{Index, IndexMut};
+//use derive_more::{Index, IndexMut};
 use ord_subset::OrdSubsetIterExt;
 
-use proc_macro::bridge::client::HandleCounters;
 use rand::{
     distributions::{Distribution, Standard},
     random, Rng,
 };
-use std::cmp;
+//use std::cmp;
 use std::collections::HashMap;
-use std::fmt;
-use std::thread::AccessError;
+//use std::fmt;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 struct State(isize, isize);
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum Action {
-    left,
-    right,
-    up,
-    down,
+    Left,
+    Right,
+    Up,
+    Down,
 }
 
 impl Distribution<Action> for Standard {
@@ -27,10 +25,10 @@ impl Distribution<Action> for Standard {
         // match rng.gen_range(0, 3) { // rand 0.5, 0.6, 0.7
         match rng.gen_range(0..=3) {
             // rand 0.8
-            0 => Action::left,
-            1 => Action::right,
-            2 => Action::up,
-            _ => Action::down,
+            0 => Action::Left,
+            1 => Action::Right,
+            2 => Action::Up,
+            _ => Action::Down,
         }
     }
 }
@@ -67,10 +65,10 @@ impl WindyGrid {
 
     fn act(&mut self, a: Action, d: isize) {
         match a {
-            Action::left => self.state.0 = 0.max(self.state.0 - d),
-            Action::right => self.state.0 = self.xsize.min(self.state.0 + d),
-            Action::up => self.state.1 = 0.max(self.state.1 - d),
-            Action::down => self.state.1 = self.ysize.min(self.state.1 + d),
+            Action::Left => self.state.0 = 0.max(self.state.0 - d),
+            Action::Right => self.state.0 = self.xsize.min(self.state.0 + d),
+            Action::Up => self.state.1 = 0.max(self.state.1 - d),
+            Action::Down => self.state.1 = self.ysize.min(self.state.1 + d),
         }
     }
 }
@@ -85,18 +83,18 @@ struct Agent<'a> {
 
 impl<'a> Agent<'a> {
     fn learn(&mut self) {
-        let mut n_rounds = 0;
         let mut terminal = false;
-        for i in 0..1000 {
+        for _ in 0..1000 {
             while !terminal {
-                n_rounds += 1;
                 let state = self.environment.state;
                 let action = self.select_action(state);
                 let (new_state, reward, terminal) = self.environment.take_action(action);
                 let q_p = self.q_table(state)[&action];
                 let q_n = self.q_table(new_state)[&self.optimal_q(new_state)];
-                self.q_table(state)[&action] =
-                    q_p + self.step_size * (reward as f64 + self.lambda * q_n - q_p);
+                self.q_table(state).insert(
+                    action,
+                    q_p + self.step_size * (reward as f64 + self.lambda * q_n - q_p),
+                );
 
                 if terminal {
                     println!("{}", reward)
@@ -106,7 +104,7 @@ impl<'a> Agent<'a> {
     }
 
     fn q_table(&self, state: State) -> HashMap<Action, f64> {
-        return self.q_table[state.0 as usize][state.1 as usize];
+        return self.q_table[state.0 as usize][state.1 as usize].clone();
     }
 
     fn select_action(&self, state: State) -> Action {
@@ -124,7 +122,7 @@ impl<'a> Agent<'a> {
             .q_table(state)
             .iter()
             .ord_subset_max_by_key(|entry| entry.1)
-            .map(|(k, v)| k)
+            .map(|(k, _v)| k)
             .unwrap();
     }
 }
