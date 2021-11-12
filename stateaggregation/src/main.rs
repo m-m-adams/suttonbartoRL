8742d83b9080e7a7094ffaac41fc690af131270b
m-m-adams <52469396+m-m-adams@users.noreply.github.com>
2021-11-11 20:05:36 -0500

    first class

---
+++ stateaggregation/src/main.rs
@@ -0,0 +1,18 @@
+use rand::{
+    distributions::{Distribution, Standard},
+    random, Rng,
+};
+
+struct RandomWalkEnvironment {
+    position: isize,
+    start: isize,
+    left_terminal: isize,
+    right_terminal: isize,
+    num_states: isize,
+}
+
+impl RandomWalkEnvironment {}
+
+fn main() {
+    println!("Hello, world!");
+}
